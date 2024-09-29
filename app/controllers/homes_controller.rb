c30b643f52853e4bb38e4ad950c8f33ea7e05fc6
marceloesparza <marceloesparza@gmail.com>
2024-09-29 16:20:33 -0300

    cambio imagenes, agrego pagina info

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   def index
   end
+  def info
+    render layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "homes/index"
+  get "homes/index", to: "homes#index"
+  get "homes/info", to: "homes#info"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
